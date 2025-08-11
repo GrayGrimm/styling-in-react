317d727e0ae767028dcdff7be5ac42949655680c
guyera <thegraygrimm@graymansurvivaltech.com>
2025-08-11 16:34:31 -0400

    Different styling options in React

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css';
 import Button1 from './components/example-1-normal-css/Button'
 import Button2 from './components/example-2-inline-css/Button'
 import Button3 from './components/example-3-styled-components/Button'
+import Button4 from './components/example-4-css-modules/Button'
+import Button5 from './components/example-5-sass/Button'
 
 const App = () => {
   return (
@@ -10,7 +12,9 @@ const App = () => {
       <Button1 buttonText="Example 1" />
       <Button2 buttonText="Example 2" />
       <Button3 buttonText="Example 3" />
-
+      <Button4 buttonText="Example 4" />
+      <Button5 buttonText="Example 5" />
+      
     </div>
   );
 };
--- src/components/example-4-css-modules/Button.jsx
+++ src/components/example-4-css-modules/Button.jsx
@@ -1,5 +1,7 @@
+import stlyes from './button.module.css';
+
 const Button = ({ buttonText }) => {
-  return <button>{buttonText}</button>;
+  return <button className={stlyes.btn}>{buttonText}</button>;
 };
 
 export default Button;
